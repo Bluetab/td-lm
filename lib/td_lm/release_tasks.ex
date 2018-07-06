532759716acf13777040c2eaabcb2fc186197685
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-07-06 09:26:43 +0200

    fix: Missing rel

--- lib/td_lm/release_tasks.ex
+++ lib/td_lm/release_tasks.ex
@@ -30,14 +30,14 @@ defmodule TdLm.ReleaseTasks do
       Enum.each(@repos, &(&1.start_link(pool_size: 1)))
 
       # Run migrations
-      Enum.each(@myapps, &run_migrations_for/1)
+      # Enum.each(@myapps, &run_migrations_for/1)
 
       # Run the seed script if it exists
-      seed_script = Path.join([priv_dir(:td_lm), "repo", "seeds.exs"])
-      if File.exists?(seed_script) do
-        IO.puts "Running seed script.."
-        Code.eval_file(seed_script)
-      end
+      # seed_script = Path.join([priv_dir(:td_lm), "repo", "seeds.exs"])
+      # if File.exists?(seed_script) do
+      #   IO.puts "Running seed script.."
+      #   Code.eval_file(seed_script)
+      # end
 
       # Signal shutdown
       #IO.puts "Success!"
