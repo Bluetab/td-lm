8909016c8ba59bff2ac532c62a652a5de7e0a243
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-07-10 08:49:52 +0200

    feat: Adding basic swagger configuration

--- lib/td_lm_web/router.ex
+++ lib/td_lm_web/router.ex
@@ -1,33 +1,59 @@
 defmodule TdLmWeb.Router do
   use TdLmWeb, :router
 
-  @endpoint_url "#{Application.get_env(:td_lm, TdLmWeb.Endpoint)[:url][:host]}:#{Application.get_env(:td_lm, TdLmWeb.Endpoint)[:url][:port]}"
+  @endpoint_url "#{Application.get_env(:td_lm, TdLmWeb.Endpoint)[:url][:host]}:#{
+                  Application.get_env(:td_lm, TdLmWeb.Endpoint)[:url][:port]
+                }"
 
   pipeline :api do
-    plug TdLm.Auth.Pipeline.Unsecure
-    plug :accepts, ["json"]
+    plug(TdLm.Auth.Pipeline.Unsecure)
+    plug(:accepts, ["json"])
   end
 
   pipeline :api_secure do
-    plug TdLm.Auth.Pipeline.Secure
+    plug(TdLm.Auth.Pipeline.Secure)
   end
 
   pipeline :api_authorized do
-    plug TdLm.Auth.CurrentUser
-    plug Guardian.Plug.LoadResource
+    plug(TdLm.Auth.CurrentUser)
+    plug(Guardian.Plug.LoadResource)
   end
 
   scope "/api/swagger" do
-    forward "/", PhoenixSwagger.Plug.SwaggerUI, otp_app: :td_lm, swagger_file: "swagger.json"
+    forward("/", PhoenixSwagger.Plug.SwaggerUI, otp_app: :td_lm, swagger_file: "swagger.json")
   end
 
   scope "/api", TdLmWeb do
-    pipe_through [:api, :api_secure, :api_authorized]
+    pipe_through([:api, :api_secure, :api_authorized])
+
     resources "/business_concept", LinkController, except: [:new, :edit, :update] do
-      get    "/fields/:concept_field_id", LinkController, :get_field
-      get    "/fields", LinkController, :get_fields
-      post   "/fields", LinkController, :add_field
-      delete "/fields/:concept_field_id", LinkController, :delete_field
+      get("/fields/:concept_field_id", LinkController, :get_field)
+      get("/fields", LinkController, :get_fields)
+      post("/fields", LinkController, :add_field)
+      delete("/fields/:concept_field_id", LinkController, :delete_field)
     end
+  end
+
+  def swagger_info do
+    %{
+      schemes: ["http"],
+      info: %{
+        version: "1.0",
+        title: "TdLm"
+      },
+      host: @endpoint_url,
+      securityDefinitions: %{
+        bearer: %{
+          type: "apiKey",
+          name: "Authorization",
+          in: "header"
+        }
+      },
+      security: [
+        %{
+          bearer: []
+        }
+      ]
+    }
   end
 end
