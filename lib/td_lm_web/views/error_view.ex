b1566d654537482cd23404e8966688605276609f
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-07-10 12:23:20 +0200

    fix: Needed error view

--- lib/td_lm_web/views/error_view.ex
+++ lib/td_lm_web/views/error_view.ex
@@ -1,16 +1,30 @@
 defmodule TdLmWeb.ErrorView do
   use TdLmWeb, :view
   alias Phoenix.Controller
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
-  def template_not_found(template, _assigns) do
-    %{errors: %{detail: Controller.status_message_from_template(template)}}
+  def render("401.json", _assigns) do
+    %{errors: %{detail: "Invalid credentials"}}
+  end
+
+  def render("403.json", _assigns) do
+    %{errors: %{detail: "Invalid authorization"}}
+  end
+
+  def render("404.json", _assigns) do
+    %{errors: %{detail: "Not found"}}
+  end
+
+  def render("422.json", _assigns) do
+    %{errors: %{detail: "Unprocessable Entity"}}
+  end
+
+  def render("500.json", _assigns) do
+    %{errors: %{detail: "Internal server error"}}
+  end
+
+  # In case no render clause matches or no
+  # template is found, let's render it as 500
+  def template_not_found(_template, assigns) do
+    render "500.json", assigns
   end
 end
