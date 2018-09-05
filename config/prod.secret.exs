8efac5c17b177957eaee58a366602cd30c30ab50
Tom Crossland <tom.crossland@bluetab.net>
2018-09-05 12:54:12 +0200

    chore: disable deployment in old environment

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,22 +14,22 @@ config :td_lm, TdLmWeb.Endpoint,
 # Configure your database
 config :td_lm, TdLm.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "td_lm_prod",
-  hostname: "localhost",
+  username: "${DB_USER}",
+  password: "${DB_PASSWORD}",
+  database: "${DB_NAME}",
+  hostname: "${DB_HOST}",
   pool_size: 15
 
-config :td_perms, redis_uri: "redis://localhost"
+config :td_perms, redis_uri: "${REDIS_URI}"
 
-config :td_lm, :audit_service, api_service: TdLmWeb.ApiServices.HttpTdAuditService,
-  audit_host: "localhost",
-  audit_port: "4007",
+config :td_lm, :audit_service,
+  api_service: TdLmWeb.ApiServices.HttpTdAuditService,
+  audit_host: "${API_AUDIT_HOST}",
+  audit_port: "${API_AUDIT_PORT}",
   audit_domain: ""
 
-  config :td_lm, TdLm.Auth.Guardian,
-    allowed_algos: ["HS512"], # optional
-    issuer: "tdauth",
-    ttl: { 1, :hours },
-    secret_key: "SuperSecretTruedat"
-
+config :td_lm, TdLm.Auth.Guardian,
+  allowed_algos: ["HS512"],
+  issuer: "tdauth",
+  ttl: {1, :hours},
+  secret_key: "${GUARDIAN_SECRET_KEY}"
