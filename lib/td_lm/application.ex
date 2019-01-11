2921cc3ea092af1975b8cd76d407a03ed06c5a85
saul.isern <saul.isern@bluetab.net>
2019-01-11 14:18:59 +0100

    chore: bump td_perms versions

--- lib/td_lm/application.ex
+++ lib/td_lm/application.ex
@@ -16,7 +16,8 @@ defmodule TdLm.Application do
       supervisor(TdLmWeb.Endpoint, []),
       # Start your own worker by calling: TdLm.Worker.start_link(arg1, arg2, arg3)
       # worker(TdLm.Worker, [arg1, arg2, arg3]),
-      worker(TdLm.ResourceLinkLoader, [TdLm.ResourceLinkLoader])
+      worker(TdLm.ResourceLinkLoader, [TdLm.ResourceLinkLoader]),
+      worker(TdLm.RelationLoader, [TdLm.RelationLoader])
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
