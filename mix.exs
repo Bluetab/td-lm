c0a9252f74b28379b310d26426d8e64123ca9287
ignacio.hidalgo@bluetab.net <ignacio.hidalgo@bluetab.net>
2021-01-13 16:06:12 +0000

    chore: [TD-3182] Allow to use redis with password

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule TdLm.Mixfile do
       {:ex_json_schema, "~> 0.7.3"},
       {:ex_machina, "~> 2.3", only: [:test]},
       {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "4.0.0"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.9.1"},
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.12.1"},
       {:quantum, "~> 3.0"},
       {:graph, git: "https://github.com/Bluetab/graph.git", tag: "1.0.0"}
     ]
