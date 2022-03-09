177e5d6b0c73b921640b1ec3027c14737ce60e16
tom.crossland@bluetab.net <tom.crossland@bluetab.net>
2022-03-09 22:43:13 +0000

    TD-4461 bump td_cache, remove publish option

--- lib/td_lm/permissions/permissions.ex
+++ lib/td_lm/permissions/permissions.ex
@@ -7,28 +7,10 @@ defmodule TdLm.Permissions do
 
   alias TdLm.Auth.Claims
 
-  @doc """
-  Check if authenticated user has a permission in a domain.
-
-  ## Examples
-
-      iex> authorized?(%Claims{}, :create, "business_concept", 12)
-      false
-
-  """
   def authorized?(%Claims{jti: jti}, permission, resource_type, id) do
     TdCache.Permissions.has_permission?(jti, permission, resource_type, id)
   end
 
-  @doc """
-  Check if authenticated user has a any permission in a domain.
-
-  ## Examples
-
-      iex> authorized_any?(%Claims{}, [:create, :delete], "business_concept", 12)
-      false
-
-  """
   def authorized_any?(%Claims{jti: jti}, permissions, resource_type, id) do
     TdCache.Permissions.has_any_permission?(jti, permissions, resource_type, id)
   end
